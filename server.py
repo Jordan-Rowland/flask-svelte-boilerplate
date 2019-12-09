e2b605b0a847b855d63c6d169c7c5465045a1577
Jordan <36084892+Jordan-Rowland@users.noreply.github.com>
2019-12-08 17:44:35 -0800

    comments for server.py

--- server.py
+++ server.py
@@ -5,11 +5,13 @@ from flask import Flask, jsonify, send_from_directory
 
 app = Flask(__name__)
 
+# Path for our main Svelte page
 @app.route("/")
 def base():
     return send_from_directory('client/public', 'index.html')
 
 
+# Path for all the static files (compiled JS/CSS, etc.)
 @app.route("/<path:path>")
 def home(path):
     return send_from_directory('client/public', path)
